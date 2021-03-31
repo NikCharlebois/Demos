c6352f701af3e09e77a9b9663a782c355d370c0e
Nik Charlebois <nicharl@microsoft.com>
2021-03-31 19:54:36 -0400

    Update CreateAzureADApplication.ps1

--- AzureADApplicationwithARM/CreateAzureADApplication.ps1
+++ AzureADApplicationwithARM/CreateAzureADApplication.ps1
@@ -1,29 +1,11 @@
-param(
-    [Parameter(Mandatory = $false)]
-    [System.String]
-    $ApplicationName,
-
-    [Parameter(Mandatory = $false)]
-    [System.String[]]
-    $IdentifierUris,
-
-    [Parameter(Mandatory = $false)]
-    [System.String[]]
-    $DelegatedPermissions,
-
-    [Parameter(Mandatory = $false)]
-    [System.String[]]
-    $AppOnlyPermissions
-)
-
-$AADApp = Get-AzADApplication -DisplayName $ApplicationName
+$AADApp = Get-AzADApplication -DisplayName ${ENV:ApplicationName}
 
 if ($null -eq $AADApp)
 {
-    Write-Output "Couldn't find and existing Azure Active Directory Application named {$DisplayName}"
-    $AADApp = New-AzADApplication -DisplayName $ApplicationName -IdentifierUris $IdentifierUris
+    Write-Output "Couldn't find and existing Azure Active Directory Application named {${ENV:ApplicationName}}"
+    $AADApp = New-AzADApplication -DisplayName ${ENV:ApplicationName} -IdentifierUris ${ENV:IdentifierUris}
 }
 else
 {
-    Write-Output "An existing Azure Active Directory Application named {$DisplayName} already exists" 
+    Write-Output "An existing Azure Active Directory Application named {${ENV:ApplicationName}} already exists" 
 }
