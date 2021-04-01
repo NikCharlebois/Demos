fa6a29c701ab8889b6dfb25d463574e4e984f6e6
Nik Charlebois <nicharl@microsoft.com>
2021-04-01 09:32:42 -0400

    Update CreateAzureADApplication.ps1

--- AzureADApplicationwithARM/CreateAzureADApplication.ps1
+++ AzureADApplicationwithARM/CreateAzureADApplication.ps1
@@ -1,17 +1,5 @@
-<#$AADApp = Get-AzADApplication -DisplayName ${ENV:ApplicationName}
-
-if ($null -eq $AADApp)
-{
-    Write-Output "Couldn't find and existing Azure Active Directory Application named {${ENV:ApplicationName}}"
-    $AADApp = New-AzADApplication -DisplayName ${ENV:ApplicationName} -IdentifierUris ${ENV:IdentifierUris}
-}
-else
-{
-    Write-Output "An existing Azure Active Directory Application named {${ENV:ApplicationName}} already exists" 
-}#>
-
 Install-Module Microsoft.Graph.Applications -Force -Confirm:$false
 Import-Module Microsoft.Graph.Applications -Force
 
-New-MgApplication -DisplayName ${ENV:ApplicationName} `
-    -IdentifierUris ${ENV:IdentifierUris}
+$AADApp = New-MgApplication -DisplayName ${ENV:ApplicationName} `
+                            -IdentifierUris ${ENV:IdentifierUris}
