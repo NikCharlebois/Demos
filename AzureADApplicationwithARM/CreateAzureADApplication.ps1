400b836014af41a8e1e512aafcbd97513c32fcdf
Nik Charlebois <nicharl@microsoft.com>
2021-04-01 09:45:48 -0400

    Update CreateAzureADApplication.ps1

--- AzureADApplicationwithARM/CreateAzureADApplication.ps1
+++ AzureADApplicationwithARM/CreateAzureADApplication.ps1
@@ -1,5 +1,9 @@
+Write-Output "Installing {Microsoft.Graph.Applications}..."
 Install-Module Microsoft.Graph.Applications -Force -Confirm:$false
+
+Write-Output "Importing {Microsoft.Graph.Applications}..."
 Import-Module Microsoft.Graph.Applications -Force
 
+Write-Output "Creating new Application {${ENV:ApplicationName}} with URIs {${ENV:IdentifierUris}}..."
 $AADApp = New-MgApplication -DisplayName ${ENV:ApplicationName} `
                             -IdentifierUris ${ENV:IdentifierUris}
