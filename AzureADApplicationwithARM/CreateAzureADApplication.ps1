0479ca5f7cd4b7ca8adab5ef66d2be66a8215531
Nik Charlebois <nicharl@microsoft.com>
2021-04-01 12:59:06 -0400

    Update CreateAzureADApplication.ps1

--- AzureADApplicationwithARM/CreateAzureADApplication.ps1
+++ AzureADApplicationwithARM/CreateAzureADApplication.ps1
@@ -1,9 +1,13 @@
-Write-Host "Installing {Microsoft.Graph.Applications}..."
-Install-Module Microsoft.Graph.Applications -Force -Confirm:$false
+Connect-AzAccount -Identity
+<#Write-Host "Installing {Microsoft.Graph.Applications}..." -NoNewline
+Install-Module Microsoft.Graph.Applications -Force -Confirm:$false | Out-Null
+Write-Host "Done"
 
-Write-Host "Importing {Microsoft.Graph.Applications}..."
-Import-Module Microsoft.Graph.Applications -Force
+Write-Host "Importing {Microsoft.Graph.Applications}..." -NoNewline
+Import-Module Microsoft.Graph.Applications -Force | Out-Null
+Write-Host "Done"
 
-Write-Host "Creating new Application {${ENV:ApplicationName}} with URIs {${ENV:IdentifierUris}}..."
+Write-Host "Creating new Application {${ENV:ApplicationName}} with URIs {${ENV:IdentifierUris}}..." -NoNewline
 $AADApp = New-MgApplication -DisplayName ${ENV:ApplicationName} `
                             -IdentifierUris ${ENV:IdentifierUris}
+Write-Host "Done"#>
