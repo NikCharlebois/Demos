e8265d4525dc9f03d876dcd39918a82461f1e96b
Nik Charlebois <nicharl@microsoft.com>
2021-04-01 11:47:49 -0400

    Update CreateAzureADApplication.ps1

--- AzureADApplicationwithARM/CreateAzureADApplication.ps1
+++ AzureADApplicationwithARM/CreateAzureADApplication.ps1
@@ -1,24 +1,14 @@
 param(
     [string]
-    $MasterKey,
+    $ApplicationName,
 
     [string]
-    $EndpointURI,
+    $IdentifierUris,
 
     [string]
-    $DatabaseID,
+    $DelegatedPermissions,
 
     [string]
-    $ContainerID
+    $AppOnlyPermissions
 )
 Add-Type -AssemblyName System.Web
-
-<#Write-Host "Installing {Microsoft.Graph.Applications}..."
-Install-Module Microsoft.Graph.Applications -Force -Confirm:$false
-
-Write-Host "Importing {Microsoft.Graph.Applications}..."
-Import-Module Microsoft.Graph.Applications -Force
-
-Write-Host "Creating new Application {${ENV:ApplicationName}} with URIs {${ENV:IdentifierUris}}..."
-$AADApp = New-MgApplication -DisplayName ${ENV:ApplicationName} `
-                            -IdentifierUris ${ENV:IdentifierUris}#>
