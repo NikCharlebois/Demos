1bfedcebb73a8367a05a9d03d19f58b5dc52e312
Nik Charlebois <nicharl@microsoft.com>
2021-04-09 13:29:35 -0400

    Updates

---
+++ EventHub/SubscribeNotifications.ps1
@@ -0,0 +1,10 @@
+param(
+    [string] [Parameter(Mandatory=$true)] $AppId,
+    [string] [Parameter(Mandatory=$true)] $CertificateThumbprint,
+    [string] [Parameter(Mandatory=$true)] $TenantId
+)
+
+Install-Module Microsoft.Graph.ChangeNotifications -Force
+Import-Module Microsoft.Graph.ChangeNotifications -Force
+
+Connect-MgGraph @PSBoundParameters
