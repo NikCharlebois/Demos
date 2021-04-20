9a2cdb217a1db6591b2a3a28490724161948fa12
Nik Charlebois <nicharl@microsoft.com>
2021-04-20 07:47:08 -0400

    Update SubscribeNotifications.ps1

--- EventHub/SubscribeNotifications.ps1
+++ EventHub/SubscribeNotifications.ps1
@@ -29,6 +29,16 @@ Connect-MgGraph -AccessToken $AccessToken | Out-Null
 Write-Host "Done"
 
 Write-Host "Creating new Subscription..." -NoNewline
-Write-Host $($PSBoundParameters | Out-String)
-New-MgSubscription -ChangeType $ChangeType -NotificationUrl $EventHubUrl -Resource $Resource -ExpirationDateTime (([System.DateTime]::UtcNow).AddMinutes(60)) -verbose
+try
+{
+    New-MgSubscription -ChangeType $ChangeType `
+                       -NotificationUrl $EventHubUrl `
+                       -Resource $Resource `
+                       -ExpirationDateTime (([System.DateTime]::UtcNow).AddMinutes(60)) `
+                       -ErrorAction Stop
+}
+catch
+{
+    Write-Host $_
+}
 Write-Host "Done"
